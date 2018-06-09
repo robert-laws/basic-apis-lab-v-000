f1f891602aeeb22db5d0b502e954ce27eae58f97
Robert Laws <roblaws@me.com>
2018-06-09 08:17:34 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,18 +1,18 @@
 class RepositoriesController < ApplicationController
 
   def search
-    
+
   end
 
   def github_search
     begin
       @res = Faraday.get('https://developer.github.com/v3/search') do |req|
-        req.params['q'] = params[:query]
+        req.params['q'] = params[:query] + '+user:dtrupenn'
       end
 
       body = JSON.parse(@res.body)
       if @res.success?
-        
+
       else
         @error = body['message']
       end
